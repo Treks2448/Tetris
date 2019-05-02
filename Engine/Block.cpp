77ff8641f09f88f1babf75d0f6068a23717a6817
Igor <igorgras100@gmail.com>
2019-05-02 16:39:38 +0100

    Block class collision function fixed

--- Engine/Block.cpp
+++ Engine/Block.cpp
@@ -69,7 +69,7 @@ Block::Block()
 	//TODO generate the block
 }
 
-void Block::Draw(Graphics& gfx, Board& board)
+void Block::Draw(Graphics& gfx, Board& board) const
 {
 	for (Vector2D iElement : BlockCoordinates)
 	{
@@ -101,4 +101,44 @@ void Block::Move(Vector2D direction)
 	{
 		BlockCoordinates[i] += direction;
 	}
+}
+
+bool Block::IsOnTop(Block& block)
+{
+	for (int iCell = 0; iCell < BlockSize; iCell++)
+	{
+		for (int iCell_2 = 0; iCell_2 < BlockSize; iCell_2++)
+		{
+			Vector2D CellBelow = { 0.f, 1.f };
+			if (BlockCoordinates[iCell] + CellBelow == block.GetBlockCoordinates(iCell_2) )
+			{
+				bIsFalling = false;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool Block::IsAtBottom(int bottomY)
+{
+	for (int iCell = 0; iCell < BlockSize; iCell++)
+	{
+		if (int (BlockCoordinates[iCell].Y) == bottomY)
+		{
+			bIsFalling = false;
+			return true;
+		}
+	}
+	return false;
+}
+
+Vector2D Block::GetBlockCoordinates(int i) const
+{
+	return BlockCoordinates[i];
+}
+
+bool Block::IsFalling()
+{
+	return bIsFalling;
 }
--- Engine/Block.h
+++ Engine/Block.h
@@ -13,7 +13,7 @@ public:
 	Block();
 
 	/* Draws the block on the board*/
-	void Draw(Graphics& gfx, Board& board);
+	void Draw(Graphics& gfx, Board& board) const;
 
 	/* Used to set the coordinate of the center of the block */
 	void SetCenterPosition(Vector2D centerPosition);
@@ -27,6 +27,17 @@ public:
 	/* Handles movement of the brick*/
 	void Move(Vector2D direction);
 
+	/* Checks if the block is on top of another block. Returns true if this is the case*/
+	bool IsOnTop(Block& block);
+
+	/* Checks if the block*/
+	bool IsAtBottom(int bottomY);
+
+	/* Returns the coordinates of the ith element of the block*/
+	Vector2D GetBlockCoordinates(int i) const;
+
+	bool IsFalling();
+
 private:
 	/* Position of the central cell of the block */
 	Vector2D CenterPosition = { 4,3 };
@@ -35,7 +46,7 @@ private:
 	static constexpr int BlockSize = 4;
 
 	/* Shows whether the brick is falling or stationary */
-	bool bIsFalling = false;
+	bool bIsFalling = true;
 
 	/* Set of coordinates of each cell of the block*/
 	Vector2D BlockCoordinates[BlockSize];
